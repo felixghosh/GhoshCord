d37f94ebfb5a1a32f570c552061a82ee55efccb9
felixghosh <zanzaniman@gmail.com>
2022-02-06 11:09:57 +0100

    Added comments and did minor refactoring of server.c

--- server.c
+++ server.c
@@ -2,25 +2,28 @@
 #include <strings.h>
 #include <pthread.h>
 
+
+//Struct type-definitions
 typedef struct connection_t connection_t;
 typedef struct sockets_t sockets_t;
 
+//Function prototypes
 void* handle_connection(void* connfd);
-
 void* accept_connections(void* sockets);
-
 void broadcast_message(char* message, char* username);
 
-struct connection_t{
-    int connfd;
-    char* username;
+//Stuct declarations
+struct connection_t{        //Struct for a connection
+    int connfd;             //Socket file descriptor
+    char* username;         //Connection username
 };
 
-struct sockets_t{
-    int connfd;
-    int listenfd;
+struct sockets_t{           //Struct for parameters of accept_connections thread
+    int connfd;             //Connection socket file descriptor
+    int listenfd;           //Listening socket file descriptor
 };
 
+//Struct constructors
 sockets_t* new_sockets(int connfd, int listenfd){
     sockets_t* sockets = malloc(sizeof(sockets_t));
     sockets->connfd = connfd;
@@ -35,67 +38,80 @@ connection_t* new_connection(int connfd, char* username){
     return connection;
 }
 
-connection_t* connections[100];
-int nbr_connections =0;
+//Global variables
+connection_t* connections[100]; //Array of connections
+int nbr_connections = 0;         //Number of connected users
+
 
 int main(int argc, char **argv){
-    int listenfd, connfd;
-    struct sockaddr_in servaddr;
+    int listenfd, connfd;           //Listening socket & connection socket
+    struct sockaddr_in servaddr;    //Server address
     
     
-    if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) //init socket
+    //Initialize listening socket
+    if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
         err_n_die("socket error.");
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERVER_PORT);
+    //Set properties of server address
+    memset(&servaddr, 0, sizeof(servaddr));         //zero it out
+    servaddr.sin_family = AF_INET;                  //set address familiy to ip
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   //set it to listen to any address
+    servaddr.sin_port = htons(SERVER_PORT);         //set server port to 18000
 
+    //Bind server address to socket and start listening
     if((bind(listenfd, (SA *)  &servaddr, sizeof(servaddr))) < 0)
         err_n_die("bind error.");
-
     if((listen(listenfd, 10)) < 0)
         err_n_die("listen error.");
 
+    //Initialize parameter struct, declare accept_connections thread, and create the thread
     sockets_t *p_sockets = new_sockets(connfd, listenfd);
     pthread_t ac;
     pthread_create(&ac, NULL, accept_connections, p_sockets);
     
 
-    int finished = 0;
-    char query[20];
+    int finished = 0;   //Boolean for terminating main loop
+    char query[20];     //String for server admin inputs
+
+    //Main loop, reads server admin commands
     while(!finished){
-        memset(query, 0, 20);
-        scanf("%19s", query);
-        if(strcmp(query, "quit") == 0){
+        memset(query, 0, 20);                       //Zero out query
+        scanf("%19s", query);                       //Read user input
+        if(strcmp(query, "quit") == 0){             //Only command as of now is shutting down the server
             printf("\nServer shutting down!\n");
             broadcast_message("\n#####################\nSERVER SHUTTING DOWN!\n#####################\n", "");
             finished = 1;
-            break;
         }
     }
-    return 0;
+    return 0;   //Successful termination
 }
 
+//This function runs in a separate thread from main and listens for any new connecting clients
 void* accept_connections(void* p_sockets){
+    //Grab parameters from param-struct and free the pointer
     int connfd = ((sockets_t*)p_sockets)->connfd;
     int listenfd = ((sockets_t*)p_sockets)->listenfd;
     free(p_sockets);
 
-    int finished = 0;
+    int finished = 0;   //Boolean for terminating main loop
+
+    //Main loop, listens for new connetions
     while(!finished){
         struct sockaddr_in client_addr;
         socklen_t addr_len;
 
-        printf("wating for next connection on port %d\n", SERVER_PORT);
+        printf("wating for next connection on port %d\n\n", SERVER_PORT);
         fflush(stdout);
-        //this will block until a connetion is established
+
+        //This will block until a connetion is established
         connfd = accept(listenfd, (SA *) &client_addr, &addr_len);
 
+        //Grab and print the client address
         char str[40];
         inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str));
-        printf("Incomming connection! Address: %s\n", str);
-
+        printf("Incomming connection! Address: %s\n\n", str);
+        
+        //Initialize parameter pointer, declare handle_connection thread, and create the thread
         int *pclient = malloc(sizeof(int));
         *pclient = connfd;
         pthread_t t;
@@ -103,9 +119,13 @@ void* accept_connections(void* p_sockets){
     }
 }
 
+//This functions is run by several threads, one for each connection.
+//It listens to the client for messages and broadcasts them to the other connected clients.
 void* handle_connection(void* p_connfd){
+    //Grab the parameter and free the pointer
     int connfd = *((int*)p_connfd);
     free(p_connfd);
+
     char sendbuff[MAXLINE+1];
     char recvbuff[MAXLINE+1];
     char username[MAXLINE+1];       
@@ -113,9 +133,9 @@ void* handle_connection(void* p_connfd){
     memset(recvbuff, 0, MAXLINE);      
     memset(username, 0, MAXLINE);
     
-    
-    while((n = read(connfd, recvbuff, MAXLINE-1)) > 0){     //read username
-        if(recvbuff[n-1] == '\n')                           //check for end of message
+    //Read username
+    while((n = read(connfd, recvbuff, MAXLINE-1)) > 0){     
+        if(recvbuff[n-1] == '\n')       //Check for end of message
             break;
         memset(recvbuff, 0, MAXLINE);
     }
@@ -123,28 +143,35 @@ void* handle_connection(void* p_connfd){
         err_n_die("read error.");
 
 
-    recvbuff[n-1] = '\0';           //remove newline from username
-    strcpy(username, recvbuff);
-    if(strcmp(username, "") == 0){
+    recvbuff[n-1] = '\0';           //Remove newline from username
+    strcpy(username, recvbuff);     //Put the string in the username buffer
+    if(strcmp(username, "") == 0){  //Check for empty username
         printf("Username NULL is not allowed! Killing connection!\n");
         close(connfd);
         pthread_exit(NULL);
     }
+
     printf("Client connected! Username: %s\n", username);
     
-    connections[nbr_connections] = new_connection(connfd, username);    //add connection to list
+    //Add connection to list
+    connections[nbr_connections] = new_connection(connfd, username);    
     nbr_connections++;
 
+    //Broadcast that user has joined
     snprintf(sendbuff, sizeof(sendbuff), "%s has joined the server!\n", username);
     broadcast_message(sendbuff, username);
     memset(sendbuff, 0, MAXLINE);
 
+    //Send welcome message to user
     snprintf(sendbuff, sizeof(sendbuff), "Connected! Welcome to the server %s:)\n", username);
     write(connfd, sendbuff, strlen(sendbuff));
     
 
-    int finished = 0;
+    int finished = 0;   //Boolean for terminating main loop
+
+    //Main loop, reads messages from client and broadcasts them
     while(!finished){
+        //Read user message
         memset(recvbuff, 0, MAXLINE);
         while((n = read(connfd, recvbuff, MAXLINE-1)) > 0){
             fprintf(stdout, "\n%s: %s", username, recvbuff);
@@ -155,20 +182,24 @@ void* handle_connection(void* p_connfd){
         }
         if(n < 0)
             err_n_die("read error.");
-
-        if(n == 0){                     //Detect user disconnecting
+        
+        //Detect user disconnecting and broadcast this
+        if(n == 0){                     
             snprintf(sendbuff, sizeof(sendbuff), "%s has left the server!\n", username);
             broadcast_message(sendbuff, username);
             int i;
+            //Find index of user
             for(i = 0; i < nbr_connections; i++){
                 if(strcmp(connections[i]->username, username) == 0)
                     break;
             }
+            //Remove user from list of connections, and shift the array to fill the hole
             for(int j = i; j < nbr_connections-1; j++)
                 connections[j] = connections[j+1];
             nbr_connections--;
             break;
         }
+        //Prepend username to message string and broadcast to users
         char message[MAXLINE+3+strlen(username)];
         strcpy(message, username);
         strcat(message, ": ");
@@ -179,6 +210,7 @@ void* handle_connection(void* p_connfd){
     pthread_exit(NULL);
 }
 
+//Function for broadcasting message to every user except for username
 void broadcast_message(char* message, char* username){
     for(int i = 0; i < nbr_connections; i++){
         if(strcmp(connections[i]->username, username) != 0){
